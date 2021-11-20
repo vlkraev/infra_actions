eeacd0e77da3d24a67fef6e8768e44ffd62384c0
Владимир Краев <vlkraev@yandex.ru>
2021-11-20 20:46:32 +0700

    End Deploy

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,8 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('Yes! У меня получилось!')
+    return HttpResponse('Yes! Можно переходить к проекту!')
 
 
 def second_page(request):
-    return HttpResponse('Можно переходить к проекту!')
+    return HttpResponse('А это вторая страница!')
